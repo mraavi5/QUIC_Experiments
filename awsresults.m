af6975d7d2afb9e9a8d3b3f66412635185f6c230
Manohar Raavi <raavimanohar5@gmail.com>
2021-10-15 11:26:17 -0600

    updated plot data

--- awsresults.m
+++ awsresults.m
@@ -1,55 +1,59 @@
 clear all
 
-% AWS TCP/TLS 10 Samples Results
+% AWS TCP/TLS 100 Samples Results
+%dilithium5
+%falcon1024
+%rsa
 
 %US TCP/TLS
-x1 = [81.96078498	0.6316493
-82.22701814	1.524338166
-83.34518691	1.189182716];
+x1 = [119.7528563	1.793415901
+113.617438	1.415422503
+111.0002668	1.324489159];
 
 %Mumbai TCP/TLS dilithium5 falcon 1024 and RSA
-y1 = [482.3950717 4.708440349
-482.0289082 7.892418847
-487.1844588 4.752692556];
+y1 = [497.7086619	2.969827027
+499.017477	3.075794272
+501.6945362	2.463180174];
 
 %paris TCP/TLS
-z1 = [264.7936344	5.400873157
-262.5837856	8.487308597
-267.3918625	4.737065984];
+z1 = [297.1406987	3.330125805
+286.9512553	2.791793216
+286.1589407	2.787988197];
 
 %seoul TCP/TLS
-a1 = [354.5339358	1.833329031
-354.919778	2.533524004
-357.2444011	1.993641634];
+a1 = [384.369677	2.245800106
+376.8391381	1.887575863
+379.8868179	1.556182627];
 
-% AWS QUIC 10 Samples Results
+% AWS QUIC 100 Samples Results
 
 %US  QUIC
-x2 = [94.89466015	9.434164585
-80.29929797	0.676950752
-79.14461761	10.33005963];
+x2 = [111.705248	3.139557506
+80.43127311	0.162469017
+48.58675409	0.08671618];
 
 %mumbai QUIC
-y2 = [205.429554	72.13957988
-158.3269144	82.83182969
-176.3735909	51.51016645];
+y2 = [232.5124565	50.34476282
+154.4235556	33.36124349
+113.3797997	24.48586264];
 
 %paris QUIC
-z2 = [301.0443637	29.95758078
-259.5917914	9.24524917
-245.2347032	35.32405245];
+z2 = [333.9946697	12.60894652
+254.6498926	4.355557575
+132.6296304	5.002401295];
 
 %seoul QUIC
-a2 = [136.6102634	49.46912745
-93.61179251	48.91756699
-119.5987241	35.35906705];
+a2 = [166.7796085	36.01295442
+94.14596558	20.337695
+82.67475178	17.84833757];
 
 
 x = categorical({'California','Mumbai','Paris','Seoul'});
 x = reordercats(x,{'California','Mumbai','Paris','Seoul'});
 
-% 1 for aws-tcp/tls 2 for aws-quic
-selection = 2
+% 1 for aws-tcp/tls 2 for aws-quic 10  samples
+% 3 for aws-tcp/tls 4 for aws-quic 100 samples (0.95%)
+selection = 3
 
 if selection == 1
     y = [83.34518691	487.1844588 267.3918625 357.2444011
@@ -72,6 +76,37 @@ elseif selection == 2
     ];
     bar(x,y)
     ylim([0 500])
+    ylabel('QUIC Handshake Time (ms)','FontSize',18)
+    xlabel('AWS Server Location','FontSize',18)
+    %set(gca, 'YScale', 'log')
+    legend({'RSA3072','Dilithium5','Falcon1024'},'FontSize',14)
+    legend('Location','northeast')
+    grid on
+    grid minor
+    axis square
+
+elseif selection == 3
+    y = [111.0002668 501.6945362 286.1589407 379.8868179
+        119.7528563	497.7086619 297.1406987 384.369677
+        113.617438	499.017477 286.9512553 376.8391381
+        ];
+    bar(x,y)
+    ylabel('TCP/TLS Handshake Time (ms)','FontSize',18)
+    xlabel('AWS Server Location','FontSize',18)
+    %set(gca, 'YScale', 'log')
+    legend({'RSA3072','Dilithium5','Falcon1024'},'FontSize',14)
+    legend('Location','northeast')
+    grid on
+    grid minor
+    axis square
+
+elseif selection == 4
+    y = [48.58675409 113.3797997 132.6296304 82.67475178
+        111.705248	232.5124565 333.9946697 166.7796085
+        80.43127311	154.4235556 254.6498926 94.14596558
+        ];
+    bar(x,y)
+    ylim([0 600])
     ylabel('QUIC Handshake Time (ms)','FontSize',18)
     xlabel('AWS Server Location','FontSize',18)
     %set(gca, 'YScale', 'log')
