9b81d9eaa9975360306cb27b549364385698ee5a
Manohar Raavi <raavimanohar5@gmail.com>
2021-11-11 09:58:15 -0700

    plots updated

---
+++ aws_all1000.m
@@ -0,0 +1,43 @@
+x = categorical({'California','Mumbai','Paris','Sao Paulo','Seoul','Sydney'});
+x = reordercats(x,{'California','Mumbai','Paris','Sao Paulo','Seoul','Sydney'});
+
+% 1 for aws-tcp/tls 2 for aws-quic 1000samples  
+selection = 2
+
+if selection == 1
+    y = [122.379169	135.2158126	144.1270909	131.9799917	117.4066642	120.0519806
+515.9635735	518.4257721	512.1977009	510.0451182	507.9833788	510.9563134
+297.3471411	311.0426342	322.4990327	312.6536183	294.6367004	297.0229267
+373.7688	380.683	382.7035	383.2937	362.3316	399.5493
+386.4140678	395.8113967	402.9860578	396.6267298	383.2759793	383.7672156
+388.1956899	387.1195414	391.9509259	394.1102777	376.3008459	386.7700467
+        ];
+    bar(x,y)
+    ylabel('TCP/TLS Handshake Time (ms)','FontSize',18)
+    xlabel('AWS Server Location','FontSize',18)
+    %set(gca, 'YScale', 'log')
+    legend({'RSA3072','Dilithium2','Dilithium3','Dilithium5','Falcon512','Falcon1024'},'FontSize',14)
+    legend('Location','northeast')
+    grid on
+    grid minor
+    axis square
+
+elseif selection == 2
+    y = [48.8415645	90.71897409	99.4038262	106.8660888	79.52564602	80.65418104
+247.1066	482.7699	485.5253	497.2999	331.7135	344.8779
+139.761	289.1693	295.385	295.5718	258.8869	257.0674
+166.6757	327.9291	329.439	334.9422	178.5011	182.7166
+174.9667	350.0423	350.5012	353.8914	199.0782	200.5917
+176.3685	338.2464	338.5213	357.08	187.5001	203.8781
+        ];
+    bar(x,y)
+    ylim([0 600])
+    ylabel('QUIC Handshake Time (ms)','FontSize',18)
+    xlabel('AWS Server Location','FontSize',18)
+    %set(gca, 'YScale', 'log')
+    legend({'RSA3072','Dilithium2','Dilithium3','Dilithium5','Falcon512','Falcon1024'},'FontSize',14)
+    legend('Location','northeast')
+    grid on
+    grid minor
+    axis square
+end
--- aws_level1_1000.m
+++ aws_level1_1000.m
@@ -5,31 +5,31 @@ x = reordercats(x,{'California','Mumbai','Paris','Sao Paulo','Seoul','Sydney'});
 selection = 2
 
 if selection == 1
-    y = [122.379169 515.963573 297.347141 373.768778 386.414068 388.19569
-        131.979992 510.045118 312.653618 383.293712 396.62673 394.110278
-120.051981 510.956313 297.022927 399.549267 383.767216 386.770047
+    y = [135.2158126 518.4257721 311.0426342 380.683 395.8113967 387.1195414
+        144.1270909 512.1977009 322.4990327 382.7035 402.9860578 391.9509259
+        117.4066642 507.9833788 294.6367004 362.3316 383.2759793 376.3008459
         ];
     bar(x,y)
     ylabel('TCP/TLS Handshake Time (ms)','FontSize',18)
     xlabel('AWS Server Location','FontSize',18)
     %set(gca, 'YScale', 'log')
-    legend({'RSA3072','Dilithium5','Falcon1024'},'FontSize',14)
+    legend({'Dilithium2','Dilithium3','Falcon512'},'FontSize',14)
     legend('Location','northeast')
     grid on
     grid minor
     axis square
 
 elseif selection == 2
-    y = [48.8415645 247.106627 139.760956 166.675668 174.966659 176.368501
-        106.8660888 497.299879 295.571812 334.942162 353.89139 357.079955
-        80.65418104 344.877913 257.067411 182.716631 200.591713 203.878116
+    y = [90.71897409 482.7699 289.1693 327.9291 350.0423 338.2464
+         99.4038262 485.5253 295.385 329.439 350.5012 338.5213
+         79.52564602 331.7135 258.8869 178.5011 199.0782 187.5001
         ];
     bar(x,y)
     ylim([0 600])
     ylabel('QUIC Handshake Time (ms)','FontSize',18)
     xlabel('AWS Server Location','FontSize',18)
     %set(gca, 'YScale', 'log')
-    legend({'RSA3072','Dilithium5','Falcon1024'},'FontSize',14)
+    legend({'Dilithium2','Dilithium3','Falcon512'},'FontSize',14)
     legend('Location','northeast')
     grid on
     grid minor
